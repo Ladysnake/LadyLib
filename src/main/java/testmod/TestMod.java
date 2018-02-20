cd16b9f83d6c74430861cdbec8c96c56f229a07b
Pyrofab <redstoneinfire@gmail.com>
2018-02-20 21:35:45 +0100

    Add test mod

---
+++ src/main/java/testmod/TestMod.java
@@ -0,0 +1,9 @@
+package testmod;
+
+import net.minecraftforge.fml.common.DummyModContainer;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "test_mod")
+public class TestMod extends DummyModContainer {
+
+}
