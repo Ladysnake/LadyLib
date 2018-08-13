7c01c7842bb120ca07e2f613df75a756e7b4d8cc
Pyrofab <redstoneinfire@gmail.com>
2018-08-13 21:06:24 +0200

    Rename greyscale shader and the bar's description

--- src/main/java/ladylib/client/shader/BaseShaders.java
+++ src/main/java/ladylib/client/shader/BaseShaders.java
@@ -9,21 +9,23 @@ import net.minecraftforge.fml.relauncher.Side;
 import static ladylib.LadyLib.MOD_ID;
 import static ladylib.client.shader.ShaderUtil.SHADER_LOCATION_PREFIX;
 
+/**
+ * A few default shaders that other mods can use
+ */
 @Mod.EventBusSubscriber(modid = MOD_ID, value = Side.CLIENT)
 public final class BaseShaders {
+    private BaseShaders() { }
 
     public static final ResourceLocation BASE_VERTEX = new ResourceLocation(MOD_ID, SHADER_LOCATION_PREFIX + "vertex_base.vsh");
     public static final ResourceLocation BASE_FRAGMENT = new ResourceLocation(MOD_ID, SHADER_LOCATION_PREFIX + "fragment_base.fsh");
 
-    public static final ResourceLocation GREYSCALE = new ResourceLocation(MOD_ID, "greyscale");
+    /**Changes the saturation of the texture based on the <tt>saturation</tt> uniform*/
+    public static final ResourceLocation SATURATION = new ResourceLocation(MOD_ID, "saturation");
 
     @SubscribeEvent
     public static void onShaderRegistry(ShaderRegistryEvent event) {
-        event.registerFragmentShader(GREYSCALE);
+        event.registerFragmentShader(SATURATION);
         event.registerFragmentShader(MWShaders.ROUNDISH);
     }
 
-    private BaseShaders() {
-        super();
-    }
 }
