01d5310435a759ea0e09040d76e0dc8d56902478
Pyrofab <redstoneinfire@gmail.com>
2018-11-19 08:42:03 +0100

    Oopsie nullability

--- src/ktIntegration/kotlin/ladylib/LLClientIntegration.kt
+++ src/ktIntegration/kotlin/ladylib/LLClientIntegration.kt
@@ -6,7 +6,7 @@ import ladylib.misc.PublicApi
 
 @PublicApi
 inline fun PostProcessShader.setupDynamicUniformsKt(index: Int = 0, dynamicSetBlock: () -> Unit) {
-    val sm = this.shaderGroup.listShaders[index].shaderManager
+    val sm = (this.shaderGroup ?: return).listShaders[index].shaderManager
     ShaderUtil.useShader(sm.program)
     dynamicSetBlock()
     ShaderUtil.revert()
