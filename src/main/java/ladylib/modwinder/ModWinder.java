97e275d659e39c4286ea15687612e04391290b7e
sschr15 <steven.sschr15@hotmail.com>
2019-11-17 15:36:36 -0600

    problems

--- src/main/java/ladylib/modwinder/ModWinder.java
+++ src/main/java/ladylib/modwinder/ModWinder.java
@@ -39,7 +39,7 @@ public class ModWinder {
             throw new AssertionError(e); // Oi this is hardcoded
         }
         if (!foundList) {
-            System.out.println("Error while fetching mod list! ModWinder was unable to access the JSON file. Are you connected to the internet? If you are, send an issue on sschr15's ModWinder page at https://github.com/sschr15/ModWinder");
+            LadyLib.LOGGER.warn("Error while fetching mod list! ModWinder was unable to access the JSON file. Are you connected to the internet? If you are, send an issue on sschr15's ModWinder page at https://github.com/sschr15/ModWinder");
         }
     }
 
